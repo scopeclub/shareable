d3bc4675e4d9c8bedf34118722e69544609006e0
ch-premchand <20r21a0412@mlrinstitutions.ac.in>
2023-08-26 18:09:23 +0530

    intial template

--- main.py
+++ main.py
@@ -7,12 +7,12 @@ from webdriver_manager.chrome import ChromeDriverManager
 options = Options()
 options.page_load_strategy = 'eager'
 #options.add_argument("--headless=new")
-#driver = webdriver.Chrome(service=ChromeService(ChromeDriverManager().install()),options=options)
-service = ChromeService("./chromedriver-win64/chromedriver.exe")
+#driver = webdriver.Chrome(service=ChromeService(ChromeDriverManager().install()),options=options) // this is for auto download chrome driver
+service = ChromeService("./chromedriver-win64/chromedriver.exe") # this is for manual download chrome driver
 driver=webdriver.Chrome(service=service,options=options)
 
 driver.get("https://toffeeshare.com/")
 time.sleep(2)
-upload=driver.find_element(By.CSS_SELECTOR,"input[type='file']").send_keys("D:\ch.premchand\work\qr_code\ECE2020-24_32.xlsx")
+upload=driver.find_element(By.CSS_SELECTOR,"input[type='file']").send_keys("D:\ch.premchand\work\qr_code\README.md")
 driver.maximize_window()
 time.sleep(10)
